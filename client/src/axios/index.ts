ef40cc7b22bd254707e78e4262dcc5723f7ac024
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-09-18 19:11:39 +0700

    feat(api): add axios instance with auth token and cache-busting interceptor

---
+++ client/src/axios/index.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5296",
+  headers: {
+    "Content-Type": "application/json",
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    "Pragma": "no-cache",
+    "Expires": "0",
+  },
+  withCredentials: true,
+});
+
+// Request interceptor - Thêm token và timestamp
+api.interceptors.request.use(
+  (config) => {
+    // 1. Thêm Bearer Token
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    // 2. Thêm timestamp để ngăn cache
+    const separator = config.url?.includes('?') ? '&' : '?';
+    config.url = `${config.url}${separator}_t=${new Date().getTime()}`;
+
+    return config;
+  },
+  (error) => {
+    console.error('Request Error!');
+    return Promise.reject(error);
+  }
+);
+
+
+// hỗ trợ
+export const setAuthToken = (token: string) => {
+  if (token) {
+    localStorage.setItem('token', token);
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  }
+};
+
+export const clearAuthToken = () => {
+  localStorage.removeItem('token');
+  delete api.defaults.headers.common['Authorization'];
+};
+
+export default api;
