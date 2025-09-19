2f3e339b93bff54040676ad6e1ae41f368ad2f1b
KrySoSad <duythuc070205@gmail.com>
2025-09-20 00:15:52 +0700

    feat: add JWT security scheme to OpenAPI documentation and update role claim type

--- server/Program.cs
+++ server/Program.cs
@@ -49,6 +49,16 @@ builder.Services.AddOpenApiDocument(config =>
     config.DocumentName = "Hospital Management System API";
     config.Title = "Hospital Management System API";
     config.Version = "v1";
+
+    config.AddSecurity("JWT", new NSwag.OpenApiSecurityScheme
+    {
+        Type = NSwag.OpenApiSecuritySchemeType.ApiKey,
+        Name = "Authorization",
+        In = NSwag.OpenApiSecurityApiKeyLocation.Header,
+        Description = "Nhập: Bearer {token}"
+    });
+
+    config.OperationProcessors.Add(new NSwag.Generation.Processors.Security.AspNetCoreOperationSecurityScopeProcessor("JWT"));
 });
 
 builder.Services.AddRouting(options =>
@@ -81,7 +91,7 @@ builder.Services.AddAuthentication("Bearer")
             ValidateLifetime = true,
             ClockSkew = TimeSpan.Zero,
 
-            RoleClaimType = "role",
+            RoleClaimType = "RoleId",
         };
     });
 
