5d70b28f4ea87bd3bfcdb5d50aac3b55b8c16002
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-10-03 22:03:08 +0700

    chore: remove unnecessary utils

--- client/src/lib/toast.ts
+++
@@ -1,8 +0,0 @@
-export const defaultOptions = {
-  position: "top-center" as const,
-  autoClose: 3000,
-  hideProgressBar: false,
-  closeOnClick: true,
-  pauseOnHover: true,
-  draggable: true,
-};
