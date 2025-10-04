e51187e1739b5e2b63b7d782bc743ae01862e251
KrySoSad <duythuc070205@gmail.com>
2025-10-04 17:24:21 +0700

    refactor: improve CurrentUserId property to use Guid.TryParse for safer parsing

--- server/WebApi/Services/CurrentUserService.cs
+++ server/WebApi/Services/CurrentUserService.cs
@@ -11,6 +11,9 @@ public class CurrentUserService : ICurrentUserService
     }
 
     private ClaimsPrincipal? User => _httpContextAccessor.HttpContext?.User;
-    public Guid? CurrentUserId => User != null ? Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!) : null;
+    public Guid? CurrentUserId
+    => User != null && Guid.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out var guid)
+        ? guid
+        : (Guid?)null;
     public string RoleId => User != null ? User.FindFirstValue("RoleId")! : "";
 }
