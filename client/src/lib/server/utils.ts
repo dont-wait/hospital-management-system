38ce52ee8f567d03c8e3f08faf5f6460382b1522
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-10-05 08:31:46 +0700

    feat: add token decode handler and apply it across related files

--- client/src/lib/server/utils.ts
+++
@@ -1,9 +0,0 @@
-export function decodePayload(token: string) {
-  try {
-    const base64 = token.split(".")[1];
-    const json = atob(base64.replace(/-/g, "+").replace(/_/g, "/"));
-    return JSON.parse(json);
-  } catch {
-    return null;
-  }
-}
