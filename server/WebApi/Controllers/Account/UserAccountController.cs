497b4d71a5ada76efd92b0642a3676f0e226f8ea
KrySoSad <duythuc070205@gmail.com>
2025-10-11 01:48:39 +0700

    fix(routes): correct route attribute for UpdateUserById method

--- server/WebApi/Controllers/Account/UserAccountController.cs
+++ server/WebApi/Controllers/Account/UserAccountController.cs
@@ -66,7 +66,7 @@ public class AccountController : ControllerBase
         }
     }
 
-    [HttpPut("/patient/{patientId}")]
+    [HttpPut("patient/{patientId}")]
     [Authorize(Roles = "patient")]
     public async Task<ApiResponse<ResponseUpdatePatient>> UpdateUserById(Guid patientId, RequestUpdatePatient request)
     {
