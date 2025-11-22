16206a5b59595e35c70f8d5418d959892889153c
dont-wait <nguyentansang15042005@gmail.com>
2025-11-22 12:00:50 +0700

    feat(appointmet): add dto for appointment notification

---
+++ server/Application/Common/DTOs/Notification/AppointmentNotificationDTO.cs
@@ -0,0 +1,14 @@
+﻿public class AppointmentNotificationDTO
+{
+    public long AppointmentId { get; set; }
+    public Guid PatientId { get; set; }
+    public string PatientName { get; set; } = string.Empty;
+    public string PatientEmail { get; set; } = string.Empty;
+    public string DoctorName { get; set; } = string.Empty;
+    public string DepartmentName { get; set; } = string.Empty;
+    public string RoomName { get; set; } = string.Empty;
+    public DateOnly AppointmentDate { get; set; }
+    public TimeOnly AppointmentStartTime { get; set; }
+    public TimeOnly AppointmentEndTime { get; set; }
+    public DateTime AppointmentDateTime { get; set; }    
+}
