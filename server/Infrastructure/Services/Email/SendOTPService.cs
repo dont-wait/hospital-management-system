474f7153f7d1e1da4a414f63d08ad2446a633cba
dont-wait <nguyentansang15042005@gmail.com>
2025-11-22 17:52:16 +0700

    feat: receive email confirm info appointment

--- server/Infrastructure/Services/Email/SendOTPService.cs
+++ server/Infrastructure/Services/Email/SendOTPService.cs
@@ -7,18 +7,18 @@ public class SendOTPService : IOTPService
 
     private readonly IEmailService _emailProvider;
     private readonly IWebHostEnvironment _env;
-    private readonly string _otpTemplate;
+    private string? _otpTemplate;
 
     public SendOTPService(IEmailService emailProvider, IWebHostEnvironment env)
     {
         _emailProvider = emailProvider;
         _env = env;
-        var otpTemplatePath = Path.Combine(_env.ContentRootPath, "Resources", "EmailTemplates", "OtpTemplate.html");
-        _otpTemplate = File.ReadAllText(otpTemplatePath);
     }
 
     public async Task SendOtpEmailAsync(string to, string otp)
     {
+        var otpTemplatePath = Path.Combine(_env.ContentRootPath, "Resources", "EmailTemplates", "OtpTemplate.html");
+        _otpTemplate = File.ReadAllText(otpTemplatePath);
         string body = _otpTemplate.Replace("{{OTP}}", otp);
         await _emailProvider.SendEmailAsync(to, "Mã OTP xác thực", body, true);
     }
