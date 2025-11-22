7f78f42eaac924621eece4dfec98c75706ce2969
dont-wait <nguyentansang15042005@gmail.com>
2025-11-22 17:52:16 +0700

    feat: receive email confirm info appointment

---
+++ server/Application/Common/Utils/FormatUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+public static class FormatUtil
+{
+    public static string FormatDate(DateOnly date)
+    {
+        var culture = new CultureInfo("vi-VN");
+        var dateTime = date.ToDateTime(TimeOnly.MinValue);
+        
+        // Capitalize first letter of day name
+        var formatted = dateTime.ToString("dddd, dd/MM/yyyy", culture);
+        return char.ToUpper(formatted[0]) + formatted.Substring(1);
+    }
+
+    public static string FormatCurrency(double amount)
+    {
+        var culture = new CultureInfo("vi-VN");
+        return amount.ToString("#,##0", culture) + " VNĐ";
+    }
+}
---
+++ server/Infrastructure/Services/Email/SendEmailTemplateService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace Infrastructure.Services.Email;
+public class SendEmailTemplateService : IEmailTemplateService
+{
+
+    private readonly IEmailService _emailProvider;
+    private readonly IWebHostEnvironment _env;
+    private string? _emailTemplate;
+    
+    
+    public SendEmailTemplateService(IEmailService emailProvider, IWebHostEnvironment env)
+    {
+        _emailProvider = emailProvider;
+        _env = env;
+    }
+    
+    public async Task SendConfirmedAppointmentEmailAsync(string to, ResponseAppointmentDTO responseAppointmentDTO)
+    {
+        var emailTemplatePath = Path.Combine(_env.ContentRootPath, "Resources", "EmailTemplates", "ConfirmedAppointmentTemplate.html");
+        _emailTemplate = File.ReadAllText(emailTemplatePath);
+        
+        var genderText = responseAppointmentDTO.Gender?.ToLower() switch
+        {
+            "m" => "Nam",
+            "f" => "Nữ",
+            "o" => "Khác",
+            _ => "Chưa xác định"
+        };
+        
+        string body = _emailTemplate
+            .Replace("{{PatientName}}", responseAppointmentDTO.FullName)
+            .Replace("{{AppointmentId}}", responseAppointmentDTO.AppointmentId.ToString())
+            .Replace("{{BillingId}}", responseAppointmentDTO.BillingId.ToString())
+            .Replace("{{DoctorName}}", responseAppointmentDTO.DoctorName)
+            .Replace("{{DepartmentName}}", responseAppointmentDTO.DepartmentName)
+            .Replace("{{RoomName}}", responseAppointmentDTO.RoomName)
+            .Replace("{{AppointmentDate}}", responseAppointmentDTO.AppointmentDate.ToString("dd/MM/yyyy"))
+            .Replace("{{StartTime}}", responseAppointmentDTO.AppointmentStartTime.ToString("HH:mm"))
+            .Replace("{{EndTime}}", responseAppointmentDTO.AppointmentEndTime.ToString("HH:mm"))
+            .Replace("{{DateOfBirth}}", responseAppointmentDTO.DateOfBirth.HasValue
+                ? responseAppointmentDTO.DateOfBirth.Value.ToString("dd/MM/yyyy")
+                : "Chưa có thông tin")
+            .Replace("{{Gender}}", genderText)
+            .Replace("{{PriceOfService}}", FormatUtil.FormatCurrency(responseAppointmentDTO.PriceOfService))
+            .Replace("{{AppointmentUrl}}", $"http://localhost:3000/appointments/{responseAppointmentDTO.AppointmentId}"); //TODO: fix sau
+        await _emailProvider.SendEmailAsync(to, "Thông tin xác nhận", body, true);
+    }
+    
+}
