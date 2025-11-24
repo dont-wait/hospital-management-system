5f1b0197fa7a00d41ccf1137619fb39406f3388d
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-11-24 21:36:13 +0700

    feat(registration): validate user profile completeness before booking

--- client/src/components/patient/booking/medical-record/AddMedicalRecord.tsx
+++ client/src/components/patient/booking/medical-record/AddMedicalRecord.tsx
@@ -1,4 +1,5 @@
 import { cookies } from "next/headers";
+import Icon from "@/components/shared/Icon";
 import { PatientService } from "@/services/server";
 import { PatientUtils, DateUtils } from "@/lib/client";
 import AddMedicalButton from "./AddMedicalButton";
@@ -12,7 +13,39 @@ export default async function AddMedicalRecord() {
     await PatientService.getPatientInfo(token);
   const patient: Patient | null = authData.patient;
 
+  function hasEmptyOrWhitespace(patient: Patient): boolean {
+    return Object.values(patient).some((value) => value === null);
+  }
+
   if (!patient) return null;
+  if (patient && hasEmptyOrWhitespace(patient)) {
+    return (
+      <div className="border-2 border-mauve rounded-md p-8 bg-martinique shadow-lg mb-4">
+        <div className="flex items-center gap-4">
+          <div className="flex-1">
+            <div className="flex-shrink-0">
+              <div className="w-12 h-12 mx-auto bg-mauve rounded-full flex items-center justify-center">
+                <Icon name="Info" />
+              </div>
+            </div>
+            <h3 className="text-center text-xl my-1 font-bold text-white">
+              Hoàn thiện thông tin bệnh nhân
+            </h3>
+            <p className="text-center text-silver">
+              Vui lòng cập nhật đầy đủ thông tin cá nhân để tạo hồ sơ khám bệnh.
+            </p>
+
+            <a
+              href="/patient/update"
+              className="flex items-center text-center mt-4 gap-2 px-6 py-3 bg-east-bay text-white font-semibold rounded-lg shadow-md"
+            >
+              Cập nhật thông tin
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles["booking-box"]}>
@@ -32,7 +65,10 @@ export default async function AddMedicalRecord() {
               Ngày sinh
             </label>
             <div className={styles["medical-record-text"]}>
-              {DateUtils.getDisplayDateTime(patient.dateOfBirth, "DayMonthYear")}
+              {DateUtils.getDisplayDateTime(
+                patient.dateOfBirth,
+                "DayMonthYear",
+              )}
             </div>
           </div>
 
