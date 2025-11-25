fc284d2f54519f89a8e725944a411482f1f57935
KrySoSad <duythuc070205@gmail.com>
2025-11-25 22:31:58 +0700

    feat(slot-time): add morning and afternoon shift configurations in SlotTimeConfig

--- server/Application/Config/SlotTimeConfig.cs
+++ server/Application/Config/SlotTimeConfig.cs
@@ -3,4 +3,12 @@ public class SlotTimeConfig
 {
     public int DefaultMaxAppointments { get; set; }
     public int SlotDurationMinutes { get; set; }
+    public WorkShiftConfig MorningShift { get; set; } = new();
+    public WorkShiftConfig AfternoonShift { get; set; } = new();
+}
+
+public class WorkShiftConfig
+{
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
 }
