91e1431299394ec34b813f585d229924be2cb204
KrySoSad <duythuc070205@gmail.com>
2025-11-26 00:34:27 +0700

    refactor(ResponseTaskRegistrationDTO): remove unnecessary [Required] attributes from properties

--- server/Application/Common/DTOs/TaskItem/ResponseTaskRegistrationDTO.cs
+++ server/Application/Common/DTOs/TaskItem/ResponseTaskRegistrationDTO.cs
@@ -2,9 +2,6 @@ using System.ComponentModel.DataAnnotations;
 
 public class ResponseTaskRegistrationDTO
 {
-    [Required]
     public long TaskId { get; set; }
-    
-    [Required]
     public Guid EmployeeId { get; set; }
 }
