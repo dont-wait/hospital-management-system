98bf38b10fbde33f92bd36be193dd59040b20afe
dont-wait <nguyentansang15042005@gmail.com>
2026-03-04 20:05:19 +0700

    feat: add flake shell for dotnet environment

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [
+          pkgs.dotnet-sdk_8
+          pkgs.omnisharp-roslyn
+          pkgs.dotnet-ef
+        ];
+
+        # Fix dotnet hay bị lỗi SSL trên NixOS
+        env.DOTNET_ROOT = "${pkgs.dotnet-sdk_8}";
+      };
+    };
+}
