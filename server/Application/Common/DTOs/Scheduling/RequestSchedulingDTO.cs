7d8f2945dcb06fe03cfe971e1394ac1cc154c667
dont-wait <nguyentansang15042005@gmail.com>
2026-03-26 12:25:59 +0700

    feat(dto): update dto for fetch api serverless

--- server/Application/Common/DTOs/Scheduling/RequestSchedulingDTO.cs
+++ server/Application/Common/DTOs/Scheduling/RequestSchedulingDTO.cs
@@ -14,13 +14,47 @@ public class RequestSchedulingDTO
     [JsonPropertyName("max_days_off_per_doctor")]
     public int MaxDaysOffPerDoctor { get; set; }
 
-    [JsonPropertyName("required_doctors_per_shift")]
-    public int RequiredDoctorsPerShift { get; set; }
+    [JsonPropertyName("rooms_per_shift")]
+    public int RoomsPerShift { get; set; }
+
+    [JsonPropertyName("doctors_per_shift")]
+    public int DoctorsPerShift { get; set; }
 
     [JsonPropertyName("shifts_per_day")]
     public int ShiftsPerDay { get; set; }
 
     [JsonPropertyName("doctors")]
     public List<RequestDoctorSchedulingDTO> Doctors { get; set; } = [];
+}
+
+public class RequestDoctorSchedulingDTO
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = default!;
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = default!;
+
+    [JsonPropertyName("experiences")]
+    public int Experiences { get; set; }
+
+    [JsonPropertyName("department_id")]
+    public string DepartmentId { get; set; } = default!;
+
+    [JsonPropertyName("specialization")]
+    public string Specialization { get; set; } = default!;
+
+    [JsonPropertyName("days_off")]
+    public List<DateOnly> DaysOff { get; set; } = [];
+
+    [JsonPropertyName("preferred_extra_days")]
+    public List<DateOnly> PreferredExtraDays { get; set; } = [];
+
+    [JsonPropertyName("has_valid_license")]
+    public Boolean HasValidLicense { get; set; }
+
+    [JsonPropertyName("is_intern")]
+    public Boolean IsIntern { get; set; }
+
 }
 
