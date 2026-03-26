074b804eca5284ce6df7dd22cc8f1d170f95fe8b
dont-wait <nguyentansang15042005@gmail.com>
2026-03-26 10:20:06 +0700

    feat: add migration for add shift(morning, afternoon)

---
+++ server/Infrastructure/Migrations/20260325193418_AddShiftInTaskItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShiftInTaskItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Shift",
+                table: "tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Shift",
+                table: "tasks");
+        }
+    }
+}
