08844da58679bee6bfec0d0e0cef9171a9c845fc
dont-wait <nguyentansang15042005@gmail.com>
2026-06-23 10:42:24 +0700

    feat: update flake.nix and untrack direnv

--- server/flake.nix
+++ server/flake.nix
@@ -1,23 +1,50 @@
+
 {
+  description = "Dotnet development environment";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
     { self, nixpkgs }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      # Gộp các thành phần dotnet lại một chỗ
+      dotnet-combined =
+        with pkgs.dotnetCorePackages;
+        combinePackages [
+          sdk_8_0
+          aspnetcore_8_0
+        ];
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = [
-          pkgs.dotnet-sdk_8
-          pkgs.roslyn
-          pkgs.dotnet-ef
-          pkgs.csharp-ls
-          pkgs.sqlcmd
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          dotnet-combined
+          roslyn-ls
+          dotnet-ef
+          sqlcmd
+          # Quan trọng: Đảm bảo có bộ thư viện chuẩn cho Linux
+          icu
+          openssl
+          zlib
         ];
 
-        # Fix dotnet hay bị lỗi SSL trên NixOS
-        env.DOTNET_ROOT = "${pkgs.dotnet-sdk_8}";
+        shellHook = ''
+          export DOTNET_ROOT="${dotnet-combined}";
+          export MSBuildSDKsPath=$(ls -d ${dotnet-combined}/share/dotnet/sdk/8.0.*/Sdks | head -n 1)
+          export LD_LIBRARY_PATH="${
+            pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc
+              pkgs.openssl
+              pkgs.icu
+              pkgs.zlib
+            ]
+          }:$LD_LIBRARY_PATH"
+          export PATH="$PATH:$HOME/.dotnet/tools"
+          echo "MSBuildSDKsPath set to: $MSBuildSDKsPath"
+          echo ".NET 8 Dev Shell (Roslyn-ready) Active"
+        '';
       };
     };
 }
+
